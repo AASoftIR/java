25230c73628c7acac3f0d0c7907c49c1ac42f105
Alireza Mohebbi <aasofttests@gmail.com>
2025-05-13 00:42:36 +0330

    fixed

--- Final/TeamManagementSystem.java
+++ Final/TeamManagementSystem.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class TeamManagementSystem {
     public static void main(String[] args) {
         List<Team> teams = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
